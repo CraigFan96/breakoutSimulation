ea223ed04d5597baed461fabda09c7b4521d5b9f
DahShahar <sdahan@DESKTOP-GQ68P2B.localdomain>
2018-04-09 17:35:11 -0400

    Minor changes

--- breakout_api.py
+++ breakout_api.py
@@ -1,64 +1,70 @@
 import breakout
 
 def create_default_ball:
-	return Ball()
+    return breakout.Ball()
+
+def create_defaul_paddle:
+    return breakout.Paddle()
+
+def create_default_board:
+    return breakout.new_board()
 
 def get_state():
-	currState = {}
-	currState[xPosition] = breakout.gameState.ball.x
-	currState[yPosition] = breakout.gameState.ball.y
-	currState[speed] = breakout.gameState.ball.speed
-	#xAngle = 
-	#yAngle = 
-	#ballAcceleration = 
-	currState[xPaddlePosition] = breakout.gameState.paddle.x
-	currState[yPaddlePosition] = breakout.gameState.paddle.y
-	currState[bricks] = breakout.gameState.board
-	return currState
+    currState = {}
+    currState[xPosition] = breakout.gameState.ball.x
+    currState[yPosition] = breakout.gameState.ball.y
+    currState[speed] = breakout.gameState.ball.speed
+    #xAngle = 
+    #yAngle = 
+    #ballAcceleration = 
+    currState[xPaddlePosition] = breakout.gameState.paddle.x
+    currState[yPaddlePosition] = breakout.gameState.paddle.y
+    currState[bricks] = breakout.gameState.board
+    return currState
 """
 def is_valid_state(state):
 
 
 def set_state(state):
-	gameState.board = state.board
-	gameState.ball = state.ball
-	gameState.score = state.score
-	gameState.paddle = state.paddle
+        gameState.board = state.board
+        gameState.ball = state.ball
+        gameState.score = state.score
+        gameState.paddle = state.paddle
 """
 
 def move_paddle(x):
-	movedPaddle = breakout.gameState.paddle.x + x
-	return movedPaddle
+    movedPaddle = breakout.gameState.paddle.x + x
+    return movedPaddle
 
 def set_paddle(x):
-	#x is an int
-	breakout.gameState.paddle.x = x
-	return gameState.paddle.x
+    #x is an int
+    breakout.gameState.paddle.x = x
+    return gameState.paddle.x
 
 def move_ball(x, y):
-	gameState.ball.x = breakout.gameState.ball.x + x
-	gameState.ball.y = breakout.gameState.ball.y + y
-	return gameState
+    gameState.ball.x = breakout.gameState.ball.x + x
+    gameState.ball.y = breakout.gameState.ball.y + y
+    return gameState
 
 def get_bricks():
-	return breakout.gameState.board
+    return breakout.gameState.board
 
 def get_num_bricks():
     currBoard = gameState.board
     total = 0
-    for rowOfBlocks in currBoard:
-            for block in rowOfBlocks:
+    for colOfBlocks in currBoard:
+        for block in colOfBlocks:
             total += block
     return total
 
 def set_bricks(newBlocks):
-	#6 rows, 18 columns
-	gameState.board = newBlocks
-	return gameState.board
+    #6 rows, 18 columns
+    gameState.board = newBlocks
+    return gameState.board
 
 def set_brick(rowVal, colVal, on):
-	#b is an int, on is a boolean(could be an int)
-	if on == 0:
-		gameState.board[rowVal][colVal] = 0
-	else:
-		gameState.board[rowVal][colVal] = 1
+    #b is an int, on is a boolean(could be an int)
+    if on == 0:
+        gameState.board[rowVal][colVal] = 0
+    else:
+        gameState.board[rowVal][colVal] = 1
