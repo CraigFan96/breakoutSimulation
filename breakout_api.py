7a1ff768dd82e3c525d0456debb27f8f1cafe5cf
DahShahar <sdahan@DESKTOP-GQ68P2B.localdomain>
2018-04-08 23:38:58 -0400

    Fix small api errors

--- breakout_api.py
+++ breakout_api.py
@@ -1,14 +1,16 @@
+import breakout
+
 def get_state():
 	currState = {}
-	currState[xPosition] = gameState.ball.x
-	currState[yPosition] = gameState.ball.y
-	currState[speed] = gameState.ball.speed
+	currState[xPosition] = breakout.gameState.ball.x
+	currState[yPosition] = breakout.gameState.ball.y
+	currState[speed] = breakout.gameState.ball.speed
 	#xAngle = 
 	#yAngle = 
 	#ballAcceleration = 
-	currState[xPaddlePosition] = gameState.paddle.x
-	currState[yPaddlePosition] = gameState.paddle.y
-	currState[bricks] = gameState.board
+	currState[xPaddlePosition] = breakout.gameState.paddle.x
+	currState[yPaddlePosition] = breakout.gameState.paddle.y
+	currState[bricks] = breakout.gameState.board
 	return currState
 
 def is_valid_state(state):
@@ -18,25 +20,29 @@ def set_state(state):
 
 
 def move_paddle(x):
-	movedPaddle = gameState.paddle.x + x
+	movedPaddle = breakout.gameState.paddle.x + x
 	return movedPaddle
 	#x is an int
 
 def set_paddle(x):
 	#x is an int
-	gameState.paddle.x = x
+	breakout.gameState.paddle.x = x
 	return gameState.paddle.x
 
 def move_ball(x, y):
-	newXLocation = gameState.ball.x + x
-	newYLocation = gameState.ball.y + y
+	newXLocation = breakout.gameState.ball.x + x
+	newYLocation = breakout.gameState.ball.y + y
 
 def get_bricks():
-	return gameState.board
+	return breakout.gameState.board
 
 
 def get_num_bricks():
-
+    ans = 0
+    for column in bricks:
+        for brick in column:
+            ans += 1
+    return ans
 
 def set_bricks():
 
