e4d2b39b83045a99a32a0f3c2b07ceac2af2ca23
CraigFan96 <fancraig96@gmail.com>
2018-04-16 16:12:53 -0400

    Change API to take in a state

--- breakout_api.py
+++ breakout_api.py
@@ -3,17 +3,17 @@ import breakout
 def create_default_ball:
 	return Ball()
 
-def get_state():
+def get_state(state):
 	currState = {}
-	currState[xPosition] = gameState.ball.x
-	currState[yPosition] = gameState.ball.y
-	currState[speed] = gameState.ball.speed
+	currState[xPosition] = state.ball.x
+	currState[yPosition] = state.ball.y
+	currState[speed] = state.ball.speed
 	#xAngle = 
 	#yAngle = 
 	#ballAcceleration = 
-	currState[xPaddlePosition] = gameState.paddle.x
-	currState[yPaddlePosition] = gameState.paddle.y
-	currState[bricks] = gameState.board
+	currState[xPaddlePosition] = state.paddle.x
+	currState[yPaddlePosition] = state.paddle.y
+	currState[bricks] = state.board
 	return currState
 """
 def is_valid_state(state):
@@ -26,40 +26,40 @@ def set_state(state):
 	gameState.paddle = state.paddle
 """
 
-def move_paddle(x):
-	movedPaddle = gameState.paddle.x + x
-	return movedPaddle
-
-def set_paddle(x):
-	gameState.paddle.x = x
+def move_paddle(state, x):
+	state.paddle.x = state.paddle.x + x
 	return gameState.paddle.x
 
-def move_ball(x, y):
-	gameState.ball.x = gameState.ball.x + x
-	gameState.ball.y = gameState.ball.y + y
+def set_paddle(state, x):
+	state.paddle.x = x
+	return gameState.paddle.x
+
+def move_ball(state, x, y):
+	state.ball.x = state.ball.x + x
+	state.ball.y = state.ball.y + y
 	return gameState.ball
 
-def get_bricks():
-	return gameState.board
+def get_bricks(state):
+	return state.board
 
-def get_num_bricks():
-	currBoard = gameState.board
+def get_num_bricks(state):
+	currBoard = state.board
 	total = 0
 	for rowOfBlocks in currBoard:
 		for block in rowOfBlocks:
 		total += block
 	return total
 
-def set_bricks(newBlocks):
+def set_bricks(state, newBlocks):
 	#6 rows, 18 columns
-	gameState.board = newBlocks
-	return gameState.board
+	state.board = newBlocks
+	return state.board
 
-def set_brick(rowVal, colVal, on):
+def set_brick(state, rowVal, colVal, on):
 	#b is an int, on is a boolean(could be an int)
 	if on == 0:
-		gameState.board[rowVal][colVal] = 0
+		state.board[rowVal][colVal] = 0
 	else:
-		gameState.board[rowVal][colVal] = 1
+		state.board[rowVal][colVal] = 1
 
-	return gameState.board
+	return state.board
